6882227b47310338c65bb26746188ffa7932825c
Ming JIN <mingj2@student.unimelb.edu.au>
2018-02-12 23:56:01 +0800

    Add files via upload

---
+++ func.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 23:14:28 2018
+
+@author: kimmeen
+"""
+
+import tensorflow as tf
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    # stride [1, x_movement, y_movement, 1]
+    # Must have strides[0] = strides[3] = 1
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def conv2d_with_batch_normal(x, w):
+    tf.nn.batch_norm_with_global_normalization()
+    pass
+
+def max_pool_2x2(x):
+    # stride [1, x_movement, y_movement, 1]
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+
